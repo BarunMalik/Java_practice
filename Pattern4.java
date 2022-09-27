a4171ba6d6978eb74dfccb7cd5bbecb3114af392
PAULASTYA CHAKRABORTY <86978708+Paulastya2007@users.noreply.github.com>
2022-09-27 13:03:05 +0000

    fixed pattern 4 output

--- Pattern4.java
+++ Pattern4.java
@@ -1,14 +1,14 @@
 public class Pattern4 {
     public static void main(String args[]){ 
-        String g="ABCD",dec="CBA";int sp=0,lg=g.length(),ldec=dec.length();
+        String g="ABCD",dec="CBA";int sp=0,lg=g.length(),ldec=dec.length(),st=0;
         for(int i=0;i<4;i++){
             System.out.print(g.substring(0, lg));
             for(int j=0;j<sp;j++){
                 System.out.print(" ");
             }
-            System.out.println(dec.substring(0, ldec));lg--;sp+=2;
-            if(i>0){ldec--;}
-        }sp=4;ldec++;lg+=2;
+            System.out.println(dec.substring(st, ldec));lg--;sp+=2;
+            if(i>0){st++;}
+        }sp=4;lg+=2;ldec=1;
         for(int i=0;i<3;i++){
             System.out.print(g.substring(0,lg));
              for(int j=0;j<sp;j++){
