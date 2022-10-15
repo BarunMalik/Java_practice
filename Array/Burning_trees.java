fabdf0ac1b7813903a01c60e5770431bb69c4d8b
PAULASTYA CHAKRABORTY <86978708+Paulastya2007@users.noreply.github.com>
2022-10-15 06:44:04 +0000

    added burning trees java class

--- Array/Burning_trees.java
+++ Array/Burning_trees.java
@@ -2,7 +2,7 @@ package Array;
 import java.util.*;
 public class Burning_trees {
     int a[][];
-    
+    int r1,c1;
     Scanner sc=new Scanner(System.in);
     Burning_trees(){
         System.out.println("Enter number of rows and then columns");
@@ -13,7 +13,14 @@ public class Burning_trees {
             }
         }print();
         System.out.println("tree needed to be burnt the count starts from 0\n First row and then column");
-
+        r1=sc.nextInt();c1=sc.nextInt();
+        if(!exist(r1, c1)&&a[r1][c1]==0){
+            System.out.println("Given coordinates or tree doesnot exist");
+        }else{
+            System.out.println("The trees starts burning");
+            square(r1, c1);
+            print();
+        }
     }
     void print(){
         for(int i=0;i<a.length;i++){
@@ -24,5 +31,19 @@ public class Burning_trees {
     }
     public static void main(String args[]){
         new Burning_trees();
+    }
+    boolean exist(int x,int y){  //x for row and y for column
+        return x>=0&&y>=0&&a.length>x&&a[x].length>y;
+    }
+    void square(int x,int y){--x;--y;int idex=3;
+        if(!exist(x, y)){++x;++y;--idex;}
+        int yp=y;
+        for(int i=0;i<idex;i++){y=yp;
+            for(int j=0;j<idex;j++){
+                if(exist(x, y)&&a[x][y]!=0){
+                    a[x][y]=0;
+                }y++;
+            }x++;
+        }
     }
 }
